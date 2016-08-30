ce52eca02daa3cebc99b108610b12f02ae15ab73
Austin Walker <Austin Walker>
2016-08-30 12:05:13 -0600

    W3D2 mini-Project User Profiles Completed

---
+++ mainService.js
@@ -0,0 +1,30 @@
+angular.module('userProfiles')
+.service('mainService', function(){
+  var data =
+  [  {
+        "id": 0,
+        "first_name": "george",
+        "last_name": "bluth",
+        "avatar": "http://kyleledbetter.com/silo-3/assets/images/avatars/george_128.jpg"
+    },
+    {
+        "id": 1,
+        "first_name": "lucille",
+        "last_name": "bluth",
+        "avatar": "http://kyleledbetter.com/silo-3/assets/images/avatars/lucille_128.jpg"
+    },
+    {
+        "id": 2,
+        "first_name": "oscar",
+        "last_name": "bluth",
+        "avatar": "http://www-scf.usc.edu/~lewisal/itp104/imgs/oscarb.jpg"
+    }]
+  this.getUsers = function() {
+    return data;
+  }
+  this.toggleFavorite = function(index){
+      data[index].isFavorite = !data[index].isFavorite;
+  }
+
+
+})
